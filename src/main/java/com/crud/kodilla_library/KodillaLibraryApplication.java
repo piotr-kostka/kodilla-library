3578d81367bd0e92086920df63f44ffbcb776f6e
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-08-10 00:02:36 +0200

    First commit

---
+++ src/main/java/com/crud/kodilla_library/KodillaLibraryApplication.java
@@ -0,0 +1,14 @@
+package com.crud.kodilla_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaLibraryApplication.class, args);
+
+	}
+
+}
